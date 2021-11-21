81945ff6c13bef716159f6c4dc91cafa5b29d8e1
Craig Fox <craig@craigwfox.com>
2021-11-21 15:59:22 -0800

    Added link to about

--- src/components/header.js
+++ src/components/header.js
@@ -7,41 +7,41 @@ import headerLogo from "../images/popcorn.svg"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `hsl(var(--clr-bg))`,
+      display: "grid",
+      placeContent: "center",
+      padding: "1.5rem",
       marginBottom: `1.45rem`,
+      background: `hsl(var(--clr-bg))`,
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+    <h1 style={{ margin: 0 }}>
+      <Link
+        to="/"
+        style={{
+          color: `var(--clr-t-title)`,
+          textDecoration: `none`,
+          display: `flex`,
+          justifyContent: `center`,
+          alignItems: `center`,
+        }}
+      >
+        <img
+          src={headerLogo}
+          alt="A bucket of popcorn"
           style={{
-            color: `var(--clr-t-title)`,
-            textDecoration: `none`,
-            display: `flex`,
-            justifyContent: `center`,
-            alignItems: `center`,
+            width: `3rem`,
+            margin: `0 1.5rem 0 0`,
+            flex: `0 0 auto`,
           }}
-        >
-          <img
-            src={headerLogo}
-            alt="A bucket of popcorn"
-            style={{
-              width: `3rem`,
-              margin: `0 1.5rem 0 0`,
-              flex: `0 0 auto`,
-            }}
-          />
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
+        />
+        {siteTitle}
+      </Link>
+    </h1>
+    <nav style={{
+      padding: `1rem 0 0`
+    }}>
+      <Link to="/support/about">About</Link>
+    </nav>
   </header>
 )
 
