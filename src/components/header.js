e57bcb6104153009f2ea9da1432d95b648a089e1
Craig Fox <craig@craigwfox.com>
2021-11-22 12:45:18 -0800

    reworked header styles to use subgrid

--- src/components/header.js
+++ src/components/header.js
@@ -6,11 +6,9 @@ import headerLogo from "../images/popcorn.svg"
 
 const Header = ({ siteTitle, siteDescription }) => (
   <header className="site-header">
-    <h1>
-      <Link to="/" className="site-logo">
-        <img src={headerLogo} alt="A bucket of popcorn" className="site-logo" />
-        {siteTitle}
-      </Link>
+    <h1 className="site-logo">
+      <img src={headerLogo} alt="A bucket of popcorn" className="site-logo" />
+      <span>{siteTitle}</span>
     </h1>
     <h2>{siteDescription}</h2>
     <nav className="nav-main">
