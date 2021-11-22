484605aa0014a10d686dfc72a00364204f55fc30
Craig Fox <craig@craigwfox.com>
2021-11-22 12:36:54 -0800

    changed to only load 700 roboto cond

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,5 +6,5 @@
 
 // You can delete this file if you're not using it
 import "@fontsource/roboto"
-import "@fontsource/roboto-condensed"
+import "@fontsource/roboto-condensed/700.css"
 import "@fontsource/alfa-slab-one"
