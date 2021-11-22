62b4d0c003168465d39e63b00c2f5572af276e8c
Craig Fox <craig@craigwfox.com>
2021-11-22 17:08:19 -0600

    made a base post layout

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,16 @@
-exports.createPages = async ({ actions }) => {
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   createPage({
     path: "/using-dsg",
     component: require.resolve("./src/templates/using-dsg.js"),
     context: {},
     defer: true,
+  })
+
+  createPage({
+    path: "/somefakepage",
+    component: path.resolve("./src/components/postLayout.js"),
   })
 }
